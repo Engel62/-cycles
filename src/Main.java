5a1f9f44bb06739ad7d90fe188b1f6e9c6d2c53d
Engel62 <sr.engel@bk.ru>
2022-10-31 15:33:24 +0300

    задача  1 - 4 выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Домашнее задание 1, задача 1");
+        for(int i = 0; i < 11; i ++) {
+            System.out.println(i);
+        }
+            /// Задание 2
+            System.out.println(" Задача 2");
+            for( int a = 10; a > 0 ; a --){
+                System.out.println(a);
+            }
+        /// Задание 3
+        System.out.println(" Задача 3");
+            for (int b = 0; b < 17; b = b + 2){
+                System.out.println(b);
+            }
+        /// Задание 4
+        System.out.println(" Задача 4");
+            for (int c = 10; c > -11; c --){
+                System.out.println(c);
+            }
+
+
+        }
     }
-}
