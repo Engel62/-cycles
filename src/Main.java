2f128bda117b3f017fb174ae0d24c4a105af965f
Engel62 <sr.engel@bk.ru>
2022-10-31 15:56:51 +0300

    Задание 6, домашнее задание 2, задание 1 - 3 выпоненно

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание 1, задача 1");
-        for(int i = 0; i < 11; i ++) {
+        for (int i = 0; i < 11; i++) {
             System.out.println(i);
         }
-            /// Задание 2
-            System.out.println(" Задача 2");
-            for( int a = 10; a > 0 ; a --){
-                System.out.println(a);
-            }
+        /// Задание 2
+        System.out.println(" Задача 2");
+        for (int a = 10; a > 0; a--) {
+            System.out.println(a);
+        }
         /// Задание 3
         System.out.println(" Задача 3");
-            for (int b = 0; b < 17; b = b + 2){
-                System.out.println(b);
-            }
+        for (int b = 0; b < 17; b = b + 2) {
+            System.out.println(b);
+        }
         /// Задание 4
         System.out.println(" Задача 4");
-            for (int c = 10; c > -11; c --){
-                System.out.println(c);
-            }
-
-
+        for (int c = 10; c > -11; c--) {
+            System.out.println(c);
+        }
+        /// Задание 6, домашнее задание 2, задание 1
+        System.out.println("Задание 6, домашнее задание 2, задание 1");
+        for (int d = 1904; d < 2097; d = d + 4) {
+            System.out.println(d + " год является високосным");
+        }
+        /// Задание 2
+        System.out.println(" Задача 2");
+        for (int f = 0; f < 100; f = f + 7) {
+            System.out.println(f);
+        }
+        /// Задание 3
+        System.out.println(" Задача 3");
+        for (int g = 1; g < 600; g = g * 2) {
+            System.out.println(g);
         }
     }
+}
